eac0391f9228ed4341a37b6aa57ae2060301ebe8
DinoJay <jott88@googlemail.com>
2024-09-20 14:34:46 +0200

    added tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 
+"use client"
+
+import { People } from './lib/People';
+
 export default function Page() {
-  return <h1>Landing page</h1>
+  return <>
+    <h1>Landing page</h1>
+    <People />
+
+  </>
+
 }
 
 
